3d04c7d354f1652eadb9f2f274aeb954289b7ebb
walter <walter.wang@imagr.co>
2023-04-21 13:20:06 +1200

    post quantization jupyter notebook

---
+++ quantized_aware_training/post_quantization_transfer_learning.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# run the download_flower_dataset.sh in dataset folder first
+
+flowers_dir = "/home/walter/git/edgetpu/dataset/flower_photos"
+
+IMAGE_SIZE = 224
+BATCH_SIZE = 64
+
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255, 
+    validation_split=0.2)
+
+train_generator = datagen.flow_from_directory(
+    flowers_dir,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE, 
+    subset='training')
+
+val_generator = datagen.flow_from_directory(
+    flowers_dir,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE, 
+    subset='validation')
+
+# read each example 
+# image_batch, label_batch = next(val_generator)
+# print(image_batch.shape)
+# print(label_batch.shape)
+
+# save label file
+print (train_generator.class_indices)
+
+labels = '\n'.join(sorted(train_generator.class_indices.keys()))
+
+label_file_savepath = os.path.join(flowers_dir, 'flower_labels.txt')
+with open(label_file_savepath, 'w') as f:
+    f.write(labels)
+
+
+# transfer learning with MobileNetV2
+IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
+
+# Create the base model from the pre-trained MobileNet V2
+base_model = tf.keras.applications.MobileNetV2(
+    input_shape=IMG_SHAPE,
+    include_top=False, 
+    weights='imagenet')
+base_model.trainable = False
+
+model = tf.keras.Sequential([
+    base_model,
+    tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Dense(units=5, activation='softmax')
+])
+
+model.compile(
+    optimizer='adam', 
+    loss='categorical_crossentropy', 
+    metrics=['accuracy'])
+
+model.summary()
+print('Number of trainable weights = {}'.format(len(model.trainable_weights)))
+
+history = model.fit(train_generator,
+                    steps_per_epoch=len(train_generator), 
+                    epochs=10,
+                    validation_data=val_generator,
+                    validation_steps=len(val_generator))
+
+
+# show training acc
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
